24d827a04e2c5de8733bfbba8d8a03076aca49ce
Gaurang <shirsatgd@gmail.com>
2021-09-15 23:15:22 +0530

    [Gaurang] commit: Attendance and Full time / Part Time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var constants_1 = require("./constants");
 var Employee = /** @class */ (function () {
     function Employee() {
         // welcome message
@@ -5,6 +8,21 @@ var Employee = /** @class */ (function () {
     }
     Employee.prototype.welcomeMsg = function () {
         console.log("|*** Welcome to Empolyee Wage Computation ***|");
+    };
+    // For attandace check
+    Employee.prototype.attCheck = function () {
+        var checkfor = Math.floor((Math.random() * 10) % 3);
+        return checkfor;
+    };
+    // for full time work
+    Employee.prototype.fullTime = function () {
+        constants_1.constants.hrs = constants_1.constants.FULL_TIME_HR;
+        return constants_1.constants.hrs;
+    };
+    // for part time work
+    Employee.prototype.partTime = function () {
+        constants_1.constants.hrs = constants_1.constants.PART_TIME_HR;
+        return constants_1.constants.hrs;
     };
     return Employee;
 }());
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-
+import {constants} from './constants'
 
 class Employee{
     constructor(){
@@ -7,9 +7,24 @@ class Employee{
     }
     welcomeMsg():void {
         console.log("|*** Welcome to Empolyee Wage Computation ***|");
-            
-       
+
     }
+        // For attandace check
+        attCheck(): number{
+            let checkfor = Math.floor((Math.random() *10 ) % 3);
+            return checkfor;
+        }
+        // for full time work
+        fullTime(): number{
+            constants.hrs = constants.FULL_TIME_HR;
+            return constants.hrs;
+        }
+        // for part time work
+        partTime(): number{
+            constants.hrs = constants.PART_TIME_HR;
+            return constants.hrs;
+        }
+    
 }
 let emp = new Employee();
 emp.welcomeMsg();
